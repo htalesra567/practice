293a186b700d2c9c3cede570b813d57916375ab0
htalesra567 <165376521+htalesra567@users.noreply.github.com>
2024-10-17 23:42:29 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String args[]){
+        System.out.println("enter two numbers :");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("the sum of two numbers ="+(a+b));
+
+        sc.close();
+    }
+}
